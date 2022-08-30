609dfb872a1f5639c92a6688bc8f89524d655458
Akash Shah <91196237+CODiNBlOOD@users.noreply.github.com>
2022-08-30 20:03:07 +0530

    ok

--- C++ Data Structure/Day13/SubarrayWithGivenSum.cpp
+++ C++ Data Structure/Day13/SubarrayWithGivenSum.cpp
@@ -2,5 +2,42 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+
+
+int n,s;
+cin>>n>>s;
+int a[n];
+for(int i=0;i<n;i++){
+    cin>>a[i];
+}
+
+int i=0; j=0; st=-1; en=-1; sum=0;
+while(j<n && sum+a[j]<s) // means our start point 
+{
+    sum+=a[j];
+    j++;
+}
+
+if(sum==s){
+    cout<<sum<<endl;
+    return 0;
+}
+
+while(j<n){
+    sum+=a[j];
+    while(sum>s){
+        sum-=a[i];
+        i++;
+    }
+    if(sum==s){
+    st=i+1; // Storing starting index
+    en=j+1; // storing ending index
+    break; // So that if any value is ==s then it break the loop and print the position of index which is in st and en 
+}
+
+    j++;
+}
+
+cout<<st<<" "<<en<<" "<<endl;
+
 }
