80fde090ec000ab964fde17b58611b3262c694d1
Akash Shah <91196237+CODiNBlOOD@users.noreply.github.com>
2022-09-07 13:21:34 +0530

    done

--- C++ Data Structure/Day14/MaxSubarray.cpp
+++ C++ Data Structure/Day14/MaxSubarray.cpp
@@ -1,4 +1,6 @@
+// Online C++ compiler to run C++ program online
 #include<iostream>
+#include<climits>
 using namespace std;
 int main() {
 int n;
@@ -7,14 +9,20 @@ int arr[n];
 for(int i=0;i<n;i++){
     cin>>arr[i];
 }
+int maxsum=INT_MIN;
 
 for(int i=0;i<n;i++){
     for(int j=i;j<n;j++){
+        int sum=0;
     for(int k=i;k<=j;k++){
-cout<<arr[k]<<" ";
-}cout<<endl;
+// cout<<arr[k]<<" "; 
+// 2nd question is to print max sum of these subbarrays so we comment the cout << arr[k] and use sum to sum;
+sum+=arr[k];
+// }cout<<endl;
+    } maxsum=max(maxsum,sum);
 }
 
 }
+cout<<maxsum<<endl;
     return 0;
 }
